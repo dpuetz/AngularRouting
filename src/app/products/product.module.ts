dbcf28f6282ebaa3250ce7bc8055395ba3312177
dp <me@gmail.com>
2018-12-27 11:31:23 -0800

    add route to products

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -5,10 +5,16 @@ import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    {path: 'products', component: ProductListComponent},
+];
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [
     ProductListComponent,
